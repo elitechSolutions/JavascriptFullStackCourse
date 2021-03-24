936fe6d3a512b8a9cfa24b3ce34bea34c80d873b
Ion Iuncu <ion.iuncu@signify.com>
2021-03-24 15:07:03 +0100

    Added solutions to assignment 1. Added code for lesson 3

---
+++ lecture3/baseProject/scripts/main.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+  console.log("this script runs outside the HTML website");
+  const myParagraph = document.getElementById("myParagraph");
+  myParagraph.style.color = 'red';
+
+})
