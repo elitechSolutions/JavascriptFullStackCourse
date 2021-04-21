6da4c2f70f7462ab00c6eb8ff30f46074edbf313
Ion Iuncu <ion.iuncu@signify.com>
2021-04-21 12:46:04 +0200

    Updated lecture 6 code

--- lecture6/myFirstAPI/index.js
+++ lecture6/myFirstAPI/index.js
@@ -4,23 +4,23 @@ const cors = require('cors');
 app.use(cors())
 const myList = []
 // Returns the full list of elements
-app.get('/list', (req, res) => {
+app.get('/fruits', (req, res) => {
   res.send(myList);
 });
 
 // Adds a new element to the list
-app.post('/list', (req, res) => {
+app.post('/fruits', (req, res) => {
   const parameters = req.query;
-  if (!parameters.title || !parameters.description) {
+  if (!parameters.name || !parameters.description) {
     res.send({ error: 'Could not add element to list' })
   } else {
-    myList.push({ title: parameters.title, description: parameters.description })
+    myList.push({ name: parameters.name, description: parameters.description })
+    res.send({ message: "Element has been added" })
   }
-  res.send({ message: "Element has been added" })
 })
 
 // Deletes an element based on the ID
-app.delete('/list/:elementId', (req, res) => {
+app.delete('/fruits/:elementId', (req, res) => {
   const elementId = req.params.elementId;
   if (myList[elementId] !== undefined) {
     myList.splice(elementId, 1);
